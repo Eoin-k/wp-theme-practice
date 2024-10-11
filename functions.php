7f6d1874e07d927a7f65d4702bd49066fa65b960
Eoin-k <100050380+Eoin-k@users.noreply.github.com01~>
2024-10-11 14:57:13 +0100

    first commit

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+
+function styleLoader()
+{
+	wp_enqueue_style('main_styles', get_stylesheet_directory_uri() . '/build/index.css');
+	wp_enqueue_style('additional_styles', get_stylesheet_directory_uri() . '/build/style-index.css');
+	wp_enqueue_style('font-awesome', 'https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.6.0/css/fontawesome.min.css');
+	wp_register_style('google', 'https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap');
+	wp_enqueue_style('google');
+	wp_enqueue_script('main-university-javascript', get_theme_file_uri('/build/index.js'), array('jquery'), 1.0, true);
+}
+
+function features()
+{
+	add_theme_support('title-tag');
+	register_nav_menu('headerMenu', 'Header Menu');
+};
+
+add_action('wp_enqueue_scripts', 'styleLoader');
+add_action('after_setup_theme', 'features');
---
+++ header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html <?php language_attributes() ?>>
+
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="UTF-8">
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class() ?>>
+	<header class="site-header">
+		<div class="container">
+			<h1 class="school-logo-text float-left"><a href="<?php echo site_url() ?>"><strong>Fictional</strong>University</a></h1>
+			<span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+			<i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
+			<div class="site-header__menu-group">
+				<nav class="main-navigation">
+					<?php wp_nav_menu(["headerMenu"]) ?>
+
+					<!-- <ul class="min-list group">
+						<li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
+						<li><a href="<?php echo site_url('/programs') ?>">Programs</a></li>
+						<li><a href="<?php echo site_url('/events') ?>">Events</a></li>
+						<li><a href="<?php echo site_url('/campuses') ?>">Campuses</a></li>
+						<li><a href="<?php echo site_url('/blog') ?>">Blog</a></li>
+					</ul> -->
+				</nav>
+				<div class="site-header__util">
+					<a href="#" class="btn btn--small btn--orange float-left push-right"> Login
+					</a>
+					<a href="#" class="btn btn--small btn--dark-orange float-left ">Signup</a>
+					<span class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+				</div>
+			</div>
+		</div>
+	</header>
